ce23f554bb524b20ec6acd3539e1e6b7388f903b
Mahesh Waghmare <maheshw@bsf.io>
2020-08-29 19:10:16 +0530

    Updates

---
+++ precache-manifest.c29f2f8317e3cbae067cab4b019c7c9f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb532963d8a35c56160fb9879f37139d",
+    "url": "/reactjs-students/index.html"
+  },
+  {
+    "revision": "0aa5c9bf60df95fb4211",
+    "url": "/reactjs-students/static/css/main.ca05cd0a.chunk.css"
+  },
+  {
+    "revision": "5f5c755919b55e1d99f1",
+    "url": "/reactjs-students/static/js/2.e425270e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/reactjs-students/static/js/2.e425270e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0aa5c9bf60df95fb4211",
+    "url": "/reactjs-students/static/js/main.03e70e04.chunk.js"
+  },
+  {
+    "revision": "85d782e08b794c239964",
+    "url": "/reactjs-students/static/js/runtime-main.b792ff54.js"
+  }
+]);
